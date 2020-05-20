ce66f44e321348039a63dbd522bb36a5c0b9153f
ZemphU <timmymcallister04@gmail.com>
2020-05-20 17:01:24 -0400

    Update form.js
    This is how the new input section will be displayed, and how the radio buttons will function. It's unlikely isaudio can or will be set here. I just put it here in comments as another example.

--- js/form.js
+++ js/form.js
@@ -5,16 +5,40 @@ function isthereani() {
     var timebox = document.getElementById("itime");
     var timelabel = document.getElementById("il2");
     if (isintermission.checked === true){
-        lengthbox.style.display = "block";
-        timebox.style.display = "block";
+        lengthbox.type = "text";
+        timebox.type = "text";
         lengthlabel.style.display = "block";
         timelabel.style.display = "block";
     } else {
         lengthbox.value = lengthbox.defaultValue;
         timebox.value = timebox.defaultValue;
-        lengthbox.style.display = "none";
-        timebox.style.display = "none";
+        lengthbox.type = "hidden";
+        timebox.type = "hidden";
         lengthlabel.style.display = "none";
         timelabel.style.display = "none";
+    }
+}
+function audioorvideo() {
+    var isyoutube = document.getElementById("youtube");
+    var isaudio = document.getElementById("audio");
+    var vidID = document.getElementById("video");
+    var audiosrc = document.getElementById("afile");
+    var idlabel = document.getElementById("idl");
+    var alabel = document.getElementById("al");
+    if (isyoutube.checked === true){
+        audiosrc.value = audiosrc.defaultValue;
+        vidID.type = "text";
+        audiosrc.type = "hidden";
+        idlabel.style.display = "block";
+        alabel.style.display = "none";
+        //isaudio = ""
+    }
+    if (isaudio.checked === true){
+        vidID.value = vidID.defaultValue;
+        vidID.type = "hidden";
+        audiosrc.type = "text";
+        idlabel.style.display = "none";
+        alabel.style.display = "block";
+        //isaudio = "1"
     }
 }
